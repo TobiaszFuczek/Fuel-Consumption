f448c144bb42924880c5b1c2f35d7176cb037619
Tobiasz Fuczek <tobfuc1412@gmail.com>
2023-08-29 19:30:33 +0200

    #1 Add new def, especially extend def validate_password

--- UserAccount.py
+++ UserAccount.py
@@ -3,3 +3,23 @@ class User_Account:
         self.login = login
         self.password = password
         self.cars = []
+    def add_new_car(self, car):
+        self.cars.append(car)
+
+    def get_cars(self):
+        return self.cars
+    def validate_password(self):
+        if self.password >= 8:
+            for x in self.password:
+                if 'A'<=x<='Z':
+                    special_sign = ["!", ",", ".", "$", "%", "&", "*", "?"]
+                    invalid = False
+                    for signs in self.password:
+                        if signs not in special_sign:
+                            print(f"Please try again, use only {special_sign}")
+                            invalid = True
+                            break
+                    if not invalid:
+                        print("Valid Password")
+                else:
+                    return False
