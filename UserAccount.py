d57877a030f585a093c6b3928c650198be844da3
Tobiasz Fuczek <tobfuc1412@gmail.com>
2023-08-30 11:23:31 +0200

    #1 Improve code with class UserAccount

--- UserAccount.py
+++ UserAccount.py
@@ -1,5 +1,5 @@
 class User_Account:
-    def __init__(self, login = str, password = str,):
+    def __init__(self, login ="", password =""):
         self.login = login
         self.password = password
         self.cars = []
@@ -9,17 +9,17 @@ class User_Account:
     def get_cars(self):
         return self.cars
     def validate_password(self):
-        if self.password >= 8:
-            for x in self.password:
-                if 'A'<=x<='Z':
-                    special_sign = ["!", ",", ".", "$", "%", "&", "*", "?"]
-                    invalid = False
-                    for signs in self.password:
-                        if signs not in special_sign:
-                            print(f"Please try again, use only {special_sign}")
-                            invalid = True
-                            break
-                    if not invalid:
-                        print("Valid Password")
-                else:
-                    return False
+        while True:
+            if len(self.password) >= 8 and any(c.isupper() for c in self.password) and any(sign in self.password for sign in ["!", ",", ".", "$", "%", "&", "*", "?"]):
+                print("Password is correct")
+                break
+            else:
+                print("Password is too short or wrong, please try again")
+                self.password = input("Request Password: ")
+
+
+if __name__ == '__main__':
+    login = input("Request Login: ")
+    password = input("Request Password: ")
+    user = User_Account(login, password)
+    user.validate_password()
